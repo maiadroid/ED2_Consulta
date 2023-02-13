274ed2a177d45e67e105abfc70e3229adfae82de
Pedro Maia <pedro2maias@gmail.com>
2023-02-13 17:44:03 -0300

    Add files via upload

---
+++ anotacoesDeResumo.java
@@ -0,0 +1,38 @@
+/*
+Árvore Binária de Busca
+    1. Definição: Árvore Binária de Busca é uma estrutura de dados baseada em árvore onde cada nó tem, no máximo, dois filhos e todos os nós da subárvore esquerda são menores que o nó raiz e todos os nós da subárvore direita são maiores.
+    2. Propriedades: As propriedades da Árvore Binária de Busca incluem a garantia de que a busca, inserção e remoção de elementos ocorrem em tempo logarítmico e que a altura da árvore é geralmente balanceada.
+    3. Operações: As operações básicas da Árvore Binária de Busca incluem busca, inserção, remoção e percurso (pré-ordem, em-ordem, pós-ordem).
+    4. Balanço: Uma Árvore Binária de Busca pode ser balanceada para garantir melhor desempenho em operações de busca. Alguns exemplos de algoritmos de balanceamento incluem AVL, RBT (Árvore Vermelho-Preto) e 2-3 Árvore.
+    5. Aplicações: A Árvore Binária de Busca é amplamente utilizada em diversas aplicações, como algoritmos de classificação, indexação de dados, gerenciamento de memória, etc.
+    6. Complexidade: As operações básicas na Árvore Binária de Busca têm complexidade de tempo geralmente logarítmica, o que as torna uma escolha eficiente para muitas aplicações.
+    7. Implementação: A implementação da Árvore Binária de Busca pode ser feita usando diferentes linguagens de programação, como C, Java, Python, entre outras. É importante ter uma boa compreensão da lógica e estrutura da Árvore Binária de Busca antes de começar a implementá-la.
+
+Árvore AVL
+    1. Definição: Árvore AVL é uma variação da Árvore Binária de Busca onde a altura de cada nó é mantida balanceada para garantir um tempo de busca efetivo.
+    2. Propriedades: Uma Árvore AVL tem a propriedade de que a diferença de altura entre as subárvores de cada nó não excede 1, garantindo uma altura média logarítmica da árvore.
+    3. Operações: As operações básicas de Árvore AVL incluem busca, inserção e remoção, semelhantes às operações da Árvore Binária de Busca.
+    4. Balanço: A Árvore AVL mantém o equilíbrio por meio de rotações simples em seus nós quando a diferença de altura entre suas subárvores excede 1.
+    5. Aplicações: A Árvore AVL é amplamente utilizada em aplicações onde é necessário garantir um tempo de busca efetivo, como sistemas de informação, indexação de dados, etc.
+    6. Complexidade: As operações básicas da Árvore AVL têm complexidade de tempo geralmente logarítmica, o que as torna uma escolha eficiente para muitas aplicações.
+    7. Implementação: A implementação da Árvore AVL pode ser feita usando diferentes linguagens de programação, como C, Java, Python, entre outras. É importante ter uma boa compreensão da lógica e estrutura da Árvore AVL antes de começar a implementá-la.
+Árvore Rubro Negra
+    1. Definição: A Árvore Rubro-Negra é uma variação da Árvore Binária de Busca que garante a altura balanceada da árvore através da adição de cores (vermelho ou preto) aos nós.
+    2. Propriedades: Uma Árvore Rubro-Negra tem as seguintes propriedades: todo nó é preto ou vermelho, a raiz é sempre preta, as folhas são nós nulos sempre pretos, e não há duas folhas consecutivas vermelhas.
+    3. Operações: As operações básicas da Árvore Rubro-Negra incluem busca, inserção e remoção, semelhantes às operações da Árvore Binária de Busca.
+    4. Balanceamento: A Árvore Rubro-Negra mantém o equilíbrio ao rotacionar nós e alternar suas cores após inserções e remoções.
+    5. Aplicações: A Árvore Rubro-Negra é amplamente utilizada em aplicações onde é necessário garantir um tempo de busca efetivo, como sistemas de informação, indexação de dados, etc.
+    6. Complexidade: As operações básicas da Árvore Rubro-Negra têm complexidade de tempo geralmente logarítmica, o que as torna uma escolha eficiente para muitas aplicações.
+    7. Implementação: A implementação da Árvore Rubro-Negra pode ser feita usando diferentes linguagens de programação, como C, Java, Python, entre outras. É importante ter uma boa compreensão da lógica e estrutura da Árvore Rubro-Negra antes de começar a implementá-la.
+
+Árvore-B
+    1. Definição: A Árvore-B é uma estrutura de dados de árvore de busca que é utilizada para armazenar e recuperar informações de forma eficiente. Ela é uma variação da Árvore-Binária.
+    2. Nós: Cada nó na Árvore-B pode conter várias chaves e seus respectivos valores. O nó também pode conter ponteiros para outros nós filhos.
+    3. Propriedades: A Árvore-B tem as seguintes propriedades: a ordem da árvore é dada pelo número máximo de chaves que cada nó pode conter, as chaves são armazenadas em ordem crescente, e a árvore é balanceada para garantir a eficiência nas operações de busca.
+    4. Operações: As operações básicas da Árvore-B incluem inserção, remoção e busca, além de operações adicionais, como impressão, balanceamento, entre outras.
+    5. Balanceamento: A Árvore-B é balanceada para garantir que as operações de busca e inserção tenham tempo de execução eficiente.
+    6. Aplicações: A Árvore-B é amplamente utilizada em aplicações que requerem armazenamento e recuperação rápidos de dados, como sistemas de informação, bancos de dados, indexação de arquivos, entre outros.
+    7. Complexidade: As operações básicas da Árvore-B têm complexidade geralmente logarítmica, o que as torna uma escolha eficiente para muitas aplicações.
+    8. Implementação: A implementação da Árvore-B pode ser feita usando diferentes linguagens de programação, como C, Java, Python, entre outras. É importante ter uma boa compreensão da lógica e estrutura da Árvore-B antes de começar a implementá-la.
+
+/*
